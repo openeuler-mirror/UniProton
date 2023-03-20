b522f4008f2719b24dc867b53a35bf832c2f0946
aruiai <zhangshuanrui@huawei.com>
2023-03-20 20:14:54 +0800

    编译脚本修改
    Signed-off-by: aruiai <zhangshuanrui@huawei.com>

--- demos/hi3093/build/build_openamp.sh
+++ demos/hi3093/build/build_openamp.sh
@@ -15,10 +15,10 @@ echo "################# git clone openamp #################"
 pushd ../component
 rm -rf ./open-amp*
 git clone https://gitee.com/src-openeuler/OpenAMP.git
-mv ./OpenAMP/open-amp-2022.10.1.tar.gz .
+mv ./OpenAMP/openamp-2022.10.1.tar.gz .
 rm -rf ./OpenAMP
-tar -zxvf open-amp-2022.10.1.tar.gz
-mv ./open-amp-2022.10.1 ./open-amp
+tar -zxvf openamp-2022.10.1.tar.gz
+mv ./openamp-2022.10.1 ./open-amp
 cp UniProton-patch-for-openamp.patch ./open-amp
 cd open-amp
 patch -p1 -d . < UniProton-patch-for-openamp.patch
