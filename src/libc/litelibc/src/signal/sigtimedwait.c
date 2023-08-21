b707a68d1058822c36d6b6fda67fd1942fde0363
hourenyu <hourenyu@huawei.com>
2023-08-21 11:12:31 +0800

    signal: adjust posix signal files
    adjust posix signal files
    
    Signed-off-by: hourenyu <hourenyu@huawei.com>

--- src/libc/litelibc/src/signal/sigtimedwait.c
+++ src/libc/litelibc/src/signal/sigtimedwait.c
@@ -10,48 +10,11 @@
  * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
  * See the Mulan PSL v2 for more details.
  * Create: 2023-06-08
- * Description: posix sigwait功能实现
+ * Description: posix sigtimedwait功能实现
  */
 #include "signal.h"
 #include "prt_signal.h"
 #include "prt_posix_internal.h"
-
-int sigwait(const sigset_t *__restrict set, int *__restrict sig)
-{
-    if (set == NULL || sig == NULL) {
-        return -1;
-    }
-
-    signalInfo prtInfo = {0};
-    signalSet prtSet = set->__bits[0];
-    U32 ret = PRT_SignalWait(&prtSet, &prtInfo, OS_SIGNAL_WAIT_FOREVER);
-    if (ret != OS_OK) {
-        return -1;
-    }
-
-    *sig = prtInfo.si_signo;
-
-    return 0;
-}
-
-int sigwaitinfo(const sigset_t *__restrict set, siginfo_t *__restrict info)
-{
-    if (set == NULL || info == NULL) {
-        return -1;
-    }
-
-    signalInfo prtInfo = {0};
-    signalSet prtSet = set->__bits[0];
-    U32 ret = PRT_SignalWait(&prtSet, &prtInfo, OS_SIGNAL_WAIT_FOREVER);
-    if (ret != OS_OK) {
-        return -1;
-    }
-
-    info->si_signo = prtInfo.si_signo;
-    info->si_code = prtInfo.si_code;
-
-    return 0;
-}
 
 int sigtimedwait(const sigset_t *__restrict set, siginfo_t *__restrict info, const struct timespec *__restrict timeout)
 {
