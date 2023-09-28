8de1462ae41e3233f0edc5385f42bb9c9be717b8
syb <2524470353@qq.com>
2023-09-28 18:01:47 +0800

    adaption: ascend310b
    adaption ascend310b by creating files in demos/ascend310b/apps dir
    
    Signed-off-by: syb <2524470353@qq.com>

---
+++ demos/ascend310b/apps/helloworld/main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include "securec.h"
+#include "prt_config.h"
+#include "prt_config_internal.h"
+#include "prt_task.h"
+#include "test.h"
+#include "timer.h"
+#include "hwi_init.h"
+#include "prt_config.h"
+
+U8 g_memRegion00[OS_MEM_FSC_PT_SIZE];
+TskHandle g_testTskHandle;
+
+void TestTaskEntry()
+{
+    U64 n = 0;
+    while (1) {
+        PRT_Printf("[uniproton] test [%llu]\n", n);
+        n++;
+        PRT_TaskDelay(1000);
+    }
+    return;
+}
+
+U32 OsTestInit(void)
+{
+    U32 ret;
+    U8 ptNo = OS_MEM_DEFAULT_FSC_PT;
+    struct TskInitParam param = {0};
+
+    param.stackAddr = (uintptr_t)PRT_MemAllocAlign(0, ptNo, 0x8000, MEM_ADDR_ALIGN_016);
+    param.taskEntry = (TskEntryFunc)TestTaskEntry;
+    param.taskPrio = 25;
+    param.name = "TestTask";
+    param.stackSize = 0x8000;
+
+    ret = PRT_TaskCreate(&g_testTskHandle, &param);
+    if (ret) {
+        return ret;
+    }
+
+    ret = PRT_TaskResume(g_testTskHandle);
+    if (ret) {
+        return ret;
+    }
+
+    return OS_OK;
+}
+
+U32 PRT_AppInit(void)
+{
+    U32 ret;
+
+    ret = OsTestInit();
+    if (ret) {
+        return ret;
+    }
+
+    ret = TestClkStart();
+    if (ret) {
+        return ret;
+    }
+
+    return OS_OK;
+}
+
+U32 PRT_HardDrvInit(void)
+{
+    U32 ret;
+
+    ret = OsHwiInit();
+    if (ret) {
+        return ret;
+    }
+    return OS_OK;
+}
+
+void PRT_HardBootInit(void)
+{
+}
+
+S32 main(void)
+{
+    PRT_UartInit();
+    PRT_Printf("[uniproton] hello uniproton!\n");
+    return OsConfigStart();
+}
+
+extern void *__wrap_memset(void *dest, int set, U32 len)
+{
+    if (dest == NULL || len == 0) {
+        return NULL;
+    }
+
+    char *ret = (char *)dest;
+    for (int i = 0; i < len; ++i) {
+        ret[i] = set;
+    }
+    return ret;
+}
+
+extern void *__wrap_memcpy(void *dest, const void *src, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        *(char *)(dest + i) = *(char *)(src + i);
+    }
+    return dest;
+}
