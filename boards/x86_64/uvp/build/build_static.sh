f546aad4a885121f1c6f8f3991e0a4464b15b455
liuxi1234 <liuxi63@huawei.com>
2024-08-29 16:42:29 +0800

    refactoring: scripts adaptation
    scripts are managed uniformly with a single compilation entry point
    
    Signed-off-by: liuxi <liuxi63@huawei.com>

--- boards/x86_64/uvp/build/build_static.sh
+++
@@ -1,8 +0,0 @@
-pushd ./../../../../
-python build.py $1
-cp output/UniProton/lib/$1/* boards/x86_64/uvp/libs
-cp output/libboundscheck/lib/$1/* boards/x86_64/uvp/libs
-cp -r output/libc boards/x86_64/uvp/include
-cp build/uniproton_config/config_x86_uvp/prt_buildef.h boards/x86_64/uvp/include
-cp -r src/include/uapi/* boards/x86_64/uvp/include
-popd
---
+++ build/build.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+set -e
+CUR_PATH=`pwd`
+ARCH_TYPE=${1}
+CPU_TYPE=${2}
+# cortex ./build.sh cortex m4 sim helloworld
+if [[ $3 == "sim" || $3 == "SIM" ]]; then
+    SIM="_SIM_"
+    APP=${4}
+elif [ "$3" != "" ]; then
+    APP=${3}
+else
+    APP=${CPU_TYPE}
+fi
+
+export HOME_PATH=$(dirname "$PWD")
+
+function clean()
+{
+    if [ -d "${HOME_PATH}/output" ]; then
+        rm -rf ${HOME_PATH}/output
+    fi
+
+    if [ -d "${HOME_PATH}/build/logs" ]; then
+        rm -rf ${HOME_PATH}/build/logs
+    fi
+
+    if [ -d "${HOME_PATH}/build/output" ]; then
+        rm -rf ${HOME_PATH}/build/output
+    fi
+
+    if [ -d "${HOME_PATH}/build/lua" ]; then
+        rm -rf ${HOME_PATH}/build/lua
+    fi
+
+    if [ -d "${HOME_PATH}/build/modbus" ]; then
+        rm -rf ${HOME_PATH}/build/modbus
+    fi
+
+    if [ -d "${HOME_PATH}/build/libcxx" ]; then
+        rm -rf ${HOME_PATH}/build/libcxx
+    fi
+
+    if [ -d "${HOME_PATH}/build/build_forte" ]; then
+        rm -rf ${HOME_PATH}/build/build_forte
+    fi
+
+    if [ -d "${HOME_PATH}/build/tools/__pycache__" ]; then
+        rm -rf ${HOME_PATH}/build/tools/__pycache__
+    fi
+
+    if [ -d "${HOME_PATH}/build/libmetal" ]; then
+        rm -rf ${HOME_PATH}/build/libmetal
+    fi
+
+    if [ -d "${HOME_PATH}/build/open-amp" ]; then
+        rm -rf ${HOME_PATH}/build/open-amp
+    fi
+
+    find "${HOME_PATH}/build/" \( -name "*.asm" -o -name "*.bin" -o -name "*.elf" \) -type f -print0 | xargs -0 rm -f
+}
+
+# x86平台如果在oebuild上编译，需要打补丁
+function patch_toolchain()
+{
+    # 获取交叉编译工具链,统一从config.xml获取,x86需要打补丁
+    if [ "${ARCH_TYPE}" = "x86_64" ]; then
+        gcc_file=/opt/buildtools/openeuler_gcc_x86_64/bin/x86_64-openeuler-linux-gnu-gcc
+        if test -f "$gcc_file"; then
+            echo "=========================x86_64 no need patch======================="
+        else
+            cp ${HOME_PATH}/src/component/patch/UniProton-patch-for-oebuild.patch ${HOME_PATH}/build/tools
+            pushd ${HOME_PATH}/build/tools
+            patch < UniProton-patch-for-oebuild.patch
+            popd
+        fi
+    fi
+}
+
+# x86平台在oebuild平台上编译需要打patch
+function get_toolchain()
+{
+    if [[ $(find "${HOME_PATH}/build/tools" -maxdepth 1 -name "*.patch" | wc -l) -eq 0 ]]; then
+        patch_toolchain
+    fi
+
+    XPATH_EXPR="string(//project[@cpu_type=\"$CPU_TYPE\"]/platform/$CONFIG_COMPILE_PATH/text())"
+    TOOLCHAIN_PATH=$(xmllint --xpath  "$XPATH_EXPR" ./tools/config.xml)/..
+}
+
+function build_image()
+{
+    echo "================================================"
+    echo "Will build ${ARCH_TYPE}_${CPU_TYPE}"
+    echo "================================================"
+
+    if [ -d "${HOME_PATH}/build/${CPU_TYPE}" ]; then
+        rm -rf ${HOME_PATH}/build/${CPU_TYPE}
+    fi
+
+    # 获取交叉编译工具链,统一从config.xml获取
+    get_toolchain
+    echo "cross toolchain  ${TOOLCHAIN_PATH}"
+
+    XPATH_EXPR="string(//project[@cpu_type=\"$CPU_TYPE\"]/platform/kconf_dir/text())"
+    KCONF_DIR=$(xmllint --xpath  "$XPATH_EXPR" ./tools/config.xml)
+    DEFCONFIG=${HOME_PATH}/build/uniproton_config/config_${KCONF_DIR}/defconfig
+    echo "DEFCONFIG PATH  ${DEFCONFIG}"
+
+    # 组件获取
+    sh ./auxiliary/build_fetch.sh ${DEFCONFIG} ${ARCH_TYPE}
+
+    # 内核编译,删除上次生成的内核静态库以及头文件，然后重新编
+    rm -rf ${HOME_PATH}/build/output
+    pushd ${HOME_PATH}/build/tools
+        python3 build.py $CPU_TYPE
+    popd
+
+    # 组件编译
+    sh ./auxiliary/build_component.sh $TOOLCHAIN_PATH ${ARCH_TYPE} ${CPU_TYPE} ${KCONF_DIR} ${DEFCONFIG}
+
+    # 具体形态单板编译
+    export TMP_DIR=$APP
+    echo "cmake start"
+    cmake -S ${HOME_PATH}/boards/${ARCH_TYPE}/${CPU_TYPE} -B $TMP_DIR -DAPP:STRING=$APP -DTOOLCHAIN_PATH:STRING=$TOOLCHAIN_PATH -DCPU_TYPE:SRTING=${CPU_TYPE} -DARCH_TYPE:SRTING=${ARCH_TYPE} -DKCONF_DIR:SRTING=${KCONF_DIR} -DSIM:STRING=$SIM
+    echo "cmake end"
+
+    pushd $TMP_DIR
+    make $APP
+    popd
+    cp ./$TMP_DIR/$APP $APP.elf
+
+    if [[ "${APP}" == "cxxTest" || "${APP}" == "eigenTest" ]]; then
+        python ./tools/bin_helper.py -f ./$APP.elf --nocopy
+    fi
+
+    if [[ ${ARCH_TYPE} == "armv8" ]]; then
+        $TOOLCHAIN_PATH/bin/aarch64-openeuler-linux-objcopy -O binary ./$APP.elf $APP.bin
+        $TOOLCHAIN_PATH/bin/aarch64-openeuler-linux-objdump -D ./$APP.elf > $APP.asm
+    elif [ "${ARCH_TYPE}" == "cortex" ]; then
+        $TOOLCHAIN_PATH/bin/arm-none-eabi-objcopy -O binary ./$APP.elf $APP.bin
+    elif [ "${ARCH_TYPE}" == "x86_64" ]; then
+        $TOOLCHAIN_PATH/bin/x86_64-openeuler-linux-gnu-objcopy -O binary ./$APP.elf $APP.bin
+        $TOOLCHAIN_PATH/bin/x86_64-openeuler-linux-gnu-objdump -S ./$APP.elf > $APP.asm
+
+        # 编译完成，x86 oebuild平台补丁恢复
+        if [[ $(find "${HOME_PATH}/build/tools" -maxdepth 1 -name "*.patch" | wc -l) -gt 0 ]]; then
+            pushd ${HOME_PATH}/build/tools
+                patch -R < UniProton-patch-for-oebuild.patch
+                rm -f UniProton-patch-for-oebuild.patch
+            popd
+        fi
+    elif [ "${ARCH_TYPE}" == "riscv64" ]; then
+        $TOOLCHAIN_PATH/bin/riscv64-unknown-elf-objcopy -O binary ./$APP.elf $APP.bin
+        $TOOLCHAIN_PATH/bin/riscv64-unknown-elf-objdump -D ./$APP.elf > $APP.asm
+    else
+        echo "=========================no objcopy======================="
+    fi
+    rm -rf $TMP_DIR
+
+    echo "================================================"
+    echo "Build ${ARCH_TYPE}_${CPU_TYPE} successfully"
+    echo "================================================"
+}
+
+function usage()
+{
+    echo "Usage:"
+    echo "    ./build.sh arch_type cpu_type [app_name]"
+    echo "               arch_type:armv8 x86_64 cortex riscv64"
+    echo "               cpu_type:ascend310b"
+    echo "               app_name:ascend310b UniPorton_test_proxy_posix_interface"
+    echo "eg. ./build.sh armv8 ascend310b"
+    echo "    ./build.sh armv8 ascend310b UniPorton_test_proxy_posix_interface"
+    echo "    ./build.sh x86_64 uvp"
+    echo "    ./build.sh cortex m4 fatfs"
+    echo "    ./build.sh cortex m4 sim helloworld"
+    echo "    ./build.sh clean"
+}
+
+
+case ${ARCH_TYPE} in
+    "armv8")
+        CONFIG_COMPILE_PATH="compile_path_arm64"
+        build_image
+        ;;
+    "x86_64")
+        CONFIG_COMPILE_PATH="compile_path_x86"
+        build_image
+        ;;
+    "cortex")
+        CONFIG_COMPILE_PATH="compile_path_arm64"
+        build_image
+        ;;
+    "riscv64")
+        CONFIG_COMPILE_PATH="compile_path_riscv64"
+        build_image
+        ;;
+    "clean")
+        clean
+        ;;
+    "help")
+        usage
+        ;;
+    *)
+        # 默认情况，如果上面的情况都不匹配
+        usage
+        ;;
+esac
+
